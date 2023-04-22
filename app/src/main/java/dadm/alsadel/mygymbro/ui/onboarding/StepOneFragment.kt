1f89bd02954b07a672c1a9eb325b58ecb2d39cf5
jauregi13 <jon.j1305@gmail.com>
2023-04-22 20:27:14 +0200

    Step One fragment created

--- app/src/main/java/dadm/alsadel/mygymbro/ui/onboarding/StepOneFragment.kt
+++ app/src/main/java/dadm/alsadel/mygymbro/ui/onboarding/StepOneFragment.kt
@@ -1,4 +1,24 @@
 package dadm.alsadel.mygymbro.ui.onboarding
 
-class StepOneFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import dadm.alsadel.mygymbro.R
+import dadm.alsadel.mygymbro.databinding.FragmentStepOneBinding
+
+class StepOneFragment : Fragment(R.layout.fragment_step_one) {
+
+    private var _binding : FragmentStepOneBinding? = null
+    private val binding get() = _binding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentStepOneBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
