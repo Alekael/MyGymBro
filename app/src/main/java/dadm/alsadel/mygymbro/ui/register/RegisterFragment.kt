4c00be02e214a1b18a1c6387b19a417b2025fea6
jauregi13 <jon.j1305@gmail.com>
2023-04-26 16:35:35 +0200

    Login y register Fragment hechos

--- app/src/main/java/dadm/alsadel/mygymbro/ui/register/RegisterFragment.kt
+++ app/src/main/java/dadm/alsadel/mygymbro/ui/register/RegisterFragment.kt
@@ -1,4 +1,24 @@
 package dadm.alsadel.mygymbro.ui.register
 
-class RegisterFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import dadm.alsadel.mygymbro.R
+import dadm.alsadel.mygymbro.databinding.FragmentRegisterBinding
+import dadm.alsadel.mygymbro.databinding.FragmentStepFiveBinding
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    private var _binding : FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentRegisterBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
