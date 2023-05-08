f0a763a709f8d27bc40763ccccec53b643a12c48
Felipe Najson <felipe.najson@gmail.com>
2023-05-08 11:32:36 +0200

    Session Fragment created

--- app/src/main/java/dadm/alsadel/mygymbro/ui/login/LoginFragment.kt
+++ app/src/main/java/dadm/alsadel/mygymbro/ui/login/LoginFragment.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import dadm.alsadel.mygymbro.HomeActivity
 import dadm.alsadel.mygymbro.R
