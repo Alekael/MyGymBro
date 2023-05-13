e26fe17fe84b07115a9c426d765e6f3fda3a5fcf
adamrahmoun <dzka,n>
2023-05-13 15:26:21 +0200

    modification of days to List

--- app/src/main/java/dadm/alsadel/mygymbro/domain/model/User.kt
+++ app/src/main/java/dadm/alsadel/mygymbro/domain/model/User.kt
@@ -4,5 +4,5 @@ import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
 data class User(val email: String, val nickname: String, val age: Int?, val weight: Double?,
-           val height: Double?, val gender: String?, val level: String?, val interest: String?, val days: String?, val time: Double?) {
+           val height: Double?, val gender: String?, val level: String?, val interest: String?, val days: MutableList<String>?, val time: Double?) {
 }
--- app/src/main/java/dadm/alsadel/mygymbro/ui/onboarding/StepFiveFragment.kt
+++ app/src/main/java/dadm/alsadel/mygymbro/ui/onboarding/StepFiveFragment.kt
@@ -32,6 +32,7 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.Query
+import java.lang.Math.floor
 
 @AndroidEntryPoint
 class StepFiveFragment : Fragment(R.layout.fragment_step_five), ConfirmationRegisterDialog.ConfirmationDialogCallBack {
@@ -40,17 +41,18 @@ class StepFiveFragment : Fragment(R.layout.fragment_step_five), ConfirmationRegi
     private val binding get() = _binding!!
     private val viewModel : StepFiveViewModel by viewModels()
 
-    var days : String? = ""
+    val all_days = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    var days : MutableList<String> = mutableListOf()
     var duration : Double = 0.0
 
     private lateinit var reference: DatabaseReference
     private var database: FirebaseDatabase = FirebaseDatabase.getInstance()
-    private val exercise:MutableList<Exercise> = mutableListOf<Exercise>()
+    private val exercise:MutableList<Exercise> = mutableListOf()
     val muscle = "biceps"
     val api_key = "YpFI5RlK4EeVml4kF4bXrQ==ltYD4eP4o14u2kSZ"
     interface ApiService {
         @GET("exercises")
-        fun getExercises(@Query("muscle") muscle: String, @Header("X-Api-Key") apiKey: String): Call<List<Exercise>>
+        fun getExercises(@Query("muscle") muscle: String, @Query("difficulty") difficulty: String, @Header("X-Api-Key") apiKey: String): Call<List<Exercise>>
     }
 
 
@@ -61,7 +63,7 @@ class StepFiveFragment : Fragment(R.layout.fragment_step_five), ConfirmationRegi
 
 
     val apiService = service.create(ApiService::class.java)
-    val call = apiService.getExercises(muscle, api_key)
+    val call = apiService.getExercises(muscle, StepThreeFragment.StepThreeCompanion.level, api_key)
 
 
 
@@ -69,7 +71,7 @@ class StepFiveFragment : Fragment(R.layout.fragment_step_five), ConfirmationRegi
         super.onViewCreated(view, savedInstanceState)
 
         _binding = FragmentStepFiveBinding.bind(view)
-
+        val number_exercises = floor((duration * days.size) / 10)
 
         Log.d("TAG", "Precall")
         call.enqueue(object : Callback<List<Exercise>> {
@@ -102,34 +104,36 @@ class StepFiveFragment : Fragment(R.layout.fragment_step_five), ConfirmationRegi
 
         binding.btfinish.setOnClickListener{
             if (binding.checkBoxMonday.isChecked()){
-                days  += "Monday; "
+                days.add(all_days[0])
             }
             if (binding.checkBoxTuesday.isChecked()){
-                days  += "Tuesday; "
+                days.add(all_days[1])
             }
             if (binding.checkBoxWednesday.isChecked()){
-                days  += "Wednesday; "
+                days.add(all_days[2])
             }
             if (binding.checkBoxThursday.isChecked()){
-                days  += "Thursday; "
+                days.add(all_days[3])
             }
             if (binding.checkBoxFriday.isChecked()){
-                days  += "Friday; "
+                days.add(all_days[4])
             }
             if (binding.checkBoxSaturday.isChecked()){
-            days  += "Saturday; "
+                days.add(all_days[5])
         }
             if (binding.checkBoxSunday.isChecked()){
-            days  += "Sunday; "
+                days.add(all_days[6])
         }
             duration = binding.txtSessionTime.text.toString().toDouble()
 
 
-            if(days == "" || duration < 0.0){
-                days = ""
+
+
+            if(days.isEmpty() || duration < 0.0){
                 Snackbar.make(binding.root, R.string.snackStepFive, Snackbar.LENGTH_SHORT).show()
 
-            }else {
+            }else if (duration>180.0) Snackbar.make(binding.root, R.string.snackStepFiveDuration, Snackbar.LENGTH_SHORT).show()
+            else {
                 val user = User(
                     RegisterFragment.RegisterFragmentCompanion.email, textNickName,
                     StepOneFragment.StepOneCompanion.textAge, StepOneFragment.StepOneCompanion.textWeight, StepOneFragment.StepOneCompanion.textHeight,
@@ -137,8 +141,7 @@ class StepFiveFragment : Fragment(R.layout.fragment_step_five), ConfirmationRegi
                     days, duration)
 
                 viewModel.createUser(user,RegisterFragment.RegisterFragmentCompanion.password)
-
-
+                
 
                 val plan: HashMap<String, List<Exercise>> = hashMapOf("Monday" to exercise)
                 val trainingPlan = TrainingPlan(textNickName, plan)
